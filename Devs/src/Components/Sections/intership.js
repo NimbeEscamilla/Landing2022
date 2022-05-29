01f1b50f8d6b5a6b05bf6cdb3e8f13d59cd21ad6
Nimbe Escamilla <1909062@upy.edu.mx>
2022-05-28 22:12:42 -0500

    Add Inteship component and Documents

--- Devs/src/Components/Sections/intership.js
+++ Devs/src/Components/Sections/intership.js
@@ -1,8 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Sally from '../../assets/Sally-22.png'
+import "../../Intership.css"
 
-class intership extends Component {
-    render(){
-        //code
-    }
+function Intership() {
+    return ( 
+        <section id='intership'>
+            <div className='container'>
+                <div className='container__right'>
+                    <h2 id='rosa' className='container__title'>
+                    Intership
+                    </h2>
+                        <p>
+                        Es un programa organizado por Workshop que toma lugar a lo largo del año. La principal meta del programa es detectar estudiantes con
+                        <span id='rosa'> potencial </span>
+                        y vincularlos en la industria del
+                        <span id='azul'> software.</span>
+                        </p>
+                    </div>
+                    <img title='Sally-22' className='container__img' src={Sally} alt="Sally-22"></img>
+            </div>
+        </section>
+    )
 }
-export default intership;
+export default Intership
