9facae7582f7194fc13074599e49c2fa3f0f1464
Nimbe Escamilla <1909062@upy.edu.mx>
2022-05-28 21:02:03 -0500

    Final Intership, añadidos documentación, logo, etc.

--- src/Components/Sections/intership.js
+++ src/Components/Sections/intership.js
@@ -5,18 +5,20 @@ import "../../Intership.css"
 function Intership() {
     return ( 
         <section id='intership'>
-            <div className='intership_container'>
-                <h2 id='blanco' className='intership__title'>
-                 Intership
-                </h2>
-                    <p>
-                    Es un programa organizado por Workshop que toma lugar a lo largo del año. La principal meta del programa es detectar estudiantes con
-                    <span id='blanco'> potencial </span>
-                    y vincularlos en la industria del
-                    <span id='azul'> software.</span>
-                    </p>
-                </div>
-                <img title='Sally-22' className='solid__Sally-22' src={Sally} alt="Sally-22"></img>
+            <div className='container'>
+                <div className='container__right'>
+                    <h2 id='rosa' className='container__title'>
+                    Intership
+                    </h2>
+                        <p>
+                        Es un programa organizado por Workshop que toma lugar a lo largo del año. La principal meta del programa es detectar estudiantes con
+                        <span id='rosa'> potencial </span>
+                        y vincularlos en la industria del
+                        <span id='azul'> software.</span>
+                        </p>
+                    </div>
+                    <img title='Sally-22' className='container__img' src={Sally} alt="Sally-22"></img>
+            </div>
         </section>
     )
 }
